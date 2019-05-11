ee8f46bfa791626a21645c7d1effd7c9dc4c5e4f
nolan23 <robyparapat23@gmail.com>
2019-05-11 12:02:12 +0700

    add dynamic port

--- main.go
+++ main.go
@@ -176,9 +176,14 @@ func main() {
 	// }
 
 	// e.Start(viper.GetString("server.address"))
+	port, ok := os.LookupEnv("PORT")
+
+	if ok == false {
+		port = viper.GetString("server.address")
+	}
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
+	e.Logger.Fatal(e.Start(port))
 }
