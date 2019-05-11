89c43a68b54817b5fc6e0c4535c3ecefeafaccbe
nolan23 <robyparapat23@gmail.com>
2019-05-11 12:23:19 +0700

    add : in echo Start

--- main.go
+++ main.go
@@ -179,11 +179,11 @@ func main() {
 	port, ok := os.LookupEnv("PORT")
 
 	if ok == false {
-		port = ""
+		port = "3000"
 	}
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(port))
+	e.Logger.Fatal(e.Start(":" + port))
 }
