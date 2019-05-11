29747f5291f9a6d016d6445ac135bbcfaa986923
nolan23 <robyparapat23@gmail.com>
2019-05-11 11:53:41 +0700

    fix typo

--- main.go
+++ main.go
@@ -180,5 +180,5 @@ func main() {
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
 	})
-	e.Logger.Fatal(e.Start(os.Getenv"PORT"))
+	e.Logger.Fatal(e.Start(os.Getenv("PORT")))
 }
