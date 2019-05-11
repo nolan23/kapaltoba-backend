dd66ac98f0ff6bdbce4fb2503ef6f6d2a681e6b2
nolan23 <robyparapat23@gmail.com>
2019-05-11 11:05:40 +0700

    change mongo driver (due to uri issue), try to connect to mongodb atlas uri

--- main.go
+++ main.go
@@ -1,41 +1,40 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
 	"github.com/dgrijalva/jwt-go"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	mgo "gopkg.in/mgo.v2"
 
 	"github.com/labstack/echo"
-	"github.com/labstack/echo/middleware"
-
-	"github.com/nolan23/kapaltoba-backend/models"
-	_userHttpDeliver "github.com/nolan23/kapaltoba-backend/user/delivery/http"
-	_userRepo "github.com/nolan23/kapaltoba-backend/user/repository"
-	_userUsecase "github.com/nolan23/kapaltoba-backend/user/usecase"
-
-	_transactionHttpDeliver "github.com/nolan23/kapaltoba-backend/transaction/delivery/http"
-	_transactionRepo "github.com/nolan23/kapaltoba-backend/transaction/repository"
-	_transactionUsecase "github.com/nolan23/kapaltoba-backend/transaction/usecase"
-
-	_tripHttpDeliver "github.com/nolan23/kapaltoba-backend/trip/delivery/http"
-	_tripRepo "github.com/nolan23/kapaltoba-backend/trip/repository"
-	_tripUsecase "github.com/nolan23/kapaltoba-backend/trip/usecase"
 
 	"github.com/spf13/viper"
-	"github.com/zebresel-com/mongodm"
 )
 
 var serverMongo = viper.GetString(`database.host`) + ":" + viper.GetString(`database.port`)
-var dbConfig = &mongodm.Config{
-	DatabaseHosts:    []string{"127.0.0.1:27017"},
-	DatabaseName:     "kapaltoba",
-	DatabaseUser:     viper.GetString(`database.user`),
-	DatabasePassword: viper.GetString(`database.pass`),
-	DatabaseSource:   "",
-}
+var mongoURI = "mongodb+srv://roby:roby_is_the_best@cluster0-ld8yy.mongodb.net"
+var mongoOld = "mongodb://roby:roby_is_the_best@cluster0-shard-00-00-ld8yy.mongodb.net:27017,cluster0-shard-00-01-ld8yy.mongodb.net:27017,cluster0-shard-00-02-ld8yy.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true"
+var dialInfo, err = mgo.ParseURL(mongoURI)
+
+// var dbConfig = &mongodm.Config{
+// 	DatabaseHosts:    []string{"mongodb://cluster0-shard-00-00-ld8yy.mongodb.net.:27017"},
+// 	DatabaseName:     "kapaltoba",
+// 	DatabaseUser:     viper.GetString(`database.user`),
+// 	DatabasePassword: viper.GetString(`database.pass`),
+// 	DatabaseSource:   "",
+// }
+
+// var dbConfig = &mongodm.Config{
+// 	DialInfo:       dialInfo,
+// 	DatabaseSource: "",
+// }
 
 type jwtCusctomClaimns struct {
 	Name string `json:"name"`
@@ -89,38 +88,76 @@ func init() {
 }
 
 func main() {
-	var con, err = mongodm.Connect(dbConfig)
-	con.Register(&models.User{}, "user")
-	con.Register(&models.Transaction{}, "transaction")
-	con.Register(&models.Trip{}, "trip")
-	con.Register(&models.Boat{}, "boat")
+
+	// _, _, es := net.LookupSRV("mongodb", "tcp", "mongodb://cluster0-shard-00-00-ld8yy.mongodb.net")
+
+	// if es != nil {
+
+	// 	log.Fatal(es)
+
+	// }
+
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
 	if err != nil {
 		log.Fatal(err)
 	}
-	defer con.Close()
-	e := echo.New()
-	e.Use(middleware.Logger())
-	e.Use(middleware.Recover())
-	e.POST("/login", login)
-	r := e.Group("/restricted")
-	config := middleware.JWTConfig{
-		Claims:     &jwtCusctomClaimns{},
-		SigningKey: []byte("rahasia"),
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+	err = client.Connect(ctx)
+	defer client.Disconnect(ctx)
+
+	if err != nil {
+		log.Fatal(err)
 	}
-	r.Use(middleware.JWTWithConfig(config))
-	r.GET("", restricted)
-	userRepo := _userRepo.NewMongoDBUserRepository(con)
-	timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
-	userUsecase := _userUsecase.NewUserUsecase(userRepo, timeoutContext)
-	_userHttpDeliver.NewUserHttpHandler(e, userUsecase)
+	err = client.Ping(ctx, nil)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
-	transactionRepo := _transactionRepo.NewMongoDBTransactionRepository(con)
-	transactionUsecase := _transactionUsecase.NewTransactionUsecase(transactionRepo, timeoutContext)
-	_transactionHttpDeliver.NewTransactionHttpHandler(e, transactionUsecase)
+	fmt.Println("Connected to MongoDB!")
+	// database := client.Database("kapaltoba")
+	// collection := database.Collection("test")
+	// _, err = collection.InsertOne(context.Background(), bson.D{{"foo", "bar"}})
+	// if err != nil {
+	// 	log.Println(err.Error())
+	// }
+	// log.Println("Connected to MongoDB!")
 
-	tripRepo := _tripRepo.NewMongoDBTripRepository(con)
-	tripUsecase := _tripUsecase.NewTripUsecase(tripRepo, userUsecase, timeoutContext)
-	_tripHttpDeliver.NewTripHttpHandler(e, tripUsecase)
+	// var con, err = mongodm.Connect(dbConfig)
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	// log.Println("Connected to MongoDB! 2")
+	// con.Register(&models.User{}, "user")
+	// con.Register(&models.Transaction{}, "transaction")
+	// con.Register(&models.Trip{}, "trip")
+	// con.Register(&models.Boat{}, "boat")
+
+	// defer con.Close()
+	// e := echo.New()
+	// e.Use(middleware.Logger())
+	// e.Use(middleware.Recover())
+	// e.POST("/login", login)
+	// r := e.Group("/restricted")
+	// config := middleware.JWTConfig{
+	// 	Claims:     &jwtCusctomClaimns{},
+	// 	SigningKey: []byte("rahasia"),
+	// }
+	// r.Use(middleware.JWTWithConfig(config))
+	// r.GET("", restricted)
+	// userRepo := _userRepo.NewMongoDBUserRepository(con)
+	// timeoutContext := time.Duration(viper.GetInt("context.timeout")) * time.Second
+	// userUsecase := _userUsecase.NewUserUsecase(userRepo, timeoutContext)
+	// _userHttpDeliver.NewUserHttpHandler(e, userUsecase)
+
+	// transactionRepo := _transactionRepo.NewMongoDBTransactionRepository(con)
+	// transactionUsecase := _transactionUsecase.NewTransactionUsecase(transactionRepo, timeoutContext)
+	// _transactionHttpDeliver.NewTransactionHttpHandler(e, transactionUsecase)
+
+	// tripRepo := _tripRepo.NewMongoDBTripRepository(con)
+	// tripUsecase := _tripUsecase.NewTripUsecase(tripRepo, userUsecase, timeoutContext)
+	// _tripHttpDeliver.NewTripHttpHandler(e, tripUsecase)
 	// var trn *models.User
 	// trn = &models.User{
 	// 	Name:         "roby",
@@ -138,5 +175,5 @@ func main() {
 	// 	fmt.Println("error saving " + err.Error())
 	// }
 
-	e.Start(viper.GetString("server.address"))
+	// e.Start(viper.GetString("server.address"))
 }
