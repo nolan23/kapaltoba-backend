3ba577dd4733e6643715768425cf497ddbdcac0a
nolan23 <robyparapat23@gmail.com>
2019-05-12 22:12:32 +0700

    remove mongodm, modify structure

--- models/transaction.go
+++ models/transaction.go
@@ -1,12 +1,14 @@
 package models
 
 import (
-	"github.com/zebresel-com/mongodm"
+	"time"
 )
 
 type Transaction struct {
-	mongodm.DocumentBase `json:",inline" bson:",inline"`
-	User                 interface{} `json:"user" bson:"user" model:"User"`
-	Trip                 interface{} `json:"trip" bson:"trip" model:"Trip"`
-	Status               string      `json:"status" bson:"status"`
+	User       interface{} `json:"user" bson:"user" model:"User"`
+	Trip       interface{} `json:"trip" bson:"trip" model:"Trip"`
+	Status     string      `json:"status" bson:"status"`
+	CreatedAt  time.Time   `json:"createdAt" bson:"createdAt"`
+	ModifiedAt time.Time   `json:"ModifiedAt" bson:"ModifiedAt"`
+	Deleted    bool        `json:"deleted" bson:"deleted"`
 }
