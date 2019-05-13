0f6722285b192c915d5642fa5b4695ae8b5b0f6b
nolan23 <robyparapat23@gmail.com>
2019-05-13 21:43:32 +0700

    add username field

--- models/user.go
+++ models/user.go
@@ -9,12 +9,14 @@ import (
 type User struct {
 	ID           primitive.ObjectID `json:"_id" bson:"_id"`
 	Name         string             `bson:"name" json:"name"`
+	Username     string             `bson:"username" json:"username"`
 	Email        string             `bson:"email" json:"email"`
 	PhoneNumber  string             `bson:"phonenumber" json:"phonenumber"`
 	BirthDate    time.Time          `bson:"birthdate" json:"birthdate"`
 	Password     string             `bson:"password" json:"password"`
 	ImageProfile string             `bson:"imageprofile" json:"imageprofile"`
 	TripHistory  interface{}        `bson:"triphistory" json:"triphistory" model:"Trip" relation:"1n"`
+	Transactions interface{}        `bson:"transactions" json:"transactions"`
 	CreatedAt    time.Time          `json:"createdAt" bson:"createdAt"`
 	ModifiedAt   time.Time          `json:"modifiedAt" bson:"modifiedAt"`
 	Deleted      bool               `json:"deleted" bson:"deleted"`
