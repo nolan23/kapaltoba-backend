960a097eca1bf2fa70de610f9ff779c67c6d9d3d
nolan23 <robyparapat23@gmail.com>
2019-05-15 10:26:21 +0700

    add credential in user model

--- models/user.go
+++ models/user.go
@@ -8,12 +8,11 @@ import (
 
 type User struct {
 	ID           primitive.ObjectID `json:"_id" bson:"_id"`
+	Credential   interface{}        `bson:"credential" json:"credential"`
 	Name         string             `bson:"name" json:"name"`
-	Username     string             `bson:"username" json:"username"`
 	Email        string             `bson:"email" json:"email"`
 	PhoneNumber  string             `bson:"phonenumber" json:"phonenumber"`
 	BirthDate    time.Time          `bson:"birthdate" json:"birthdate"`
-	Password     string             `bson:"password" json:"password"`
 	ImageProfile string             `bson:"imageprofile" json:"imageprofile"`
 	TripHistory  interface{}        `bson:"triphistory" json:"triphistory" model:"Trip" relation:"1n"`
 	Transactions interface{}        `bson:"transactions" json:"transactions"`
