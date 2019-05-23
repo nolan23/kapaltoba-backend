43baf6a811786b73b3d2778dfd88a632e2aae3b4
nolan23 <robyparapat23@gmail.com>
2019-05-23 09:02:26 +0700

    revise bson import

--- boat/usecase.go
+++ boat/usecase.go
@@ -10,6 +10,6 @@ type Usecase interface {
 	Fetch(ctx context.Context, limit int, skip int, sort string) (res []*models.Boat, nextSkip int, err error)
 	GetByID(ctx context.Context, id string) (*models.Boat, error)
 	Update(ctx context.Context, selector interface{}, update interface{}) error
-	Store(ctx context.Context, transaction *models.Boat) error
+	Store(ctx context.Context, boat *models.Boat) error
 	Delete(ctx context.Context, id string) error
 }
